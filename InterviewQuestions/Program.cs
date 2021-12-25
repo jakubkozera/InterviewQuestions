9f826da26e25186dfdb8eae90499a58b8c0ce7ab
Jakub Kozera <jakubkozera@outlook.com>
2021-12-25 08:43:57 +0100

    Add project files.

---
+++ InterviewQuestions/Program.cs
@@ -0,0 +1,33 @@
+﻿internal abstract class AbstractClass
+{
+    public AbstractClass()
+    {
+        ProtectedField = "pf";
+    }
+
+    protected string ProtectedField;
+    public string MyProperty { get; set; }
+
+    protected abstract void FooClass();
+}
+
+internal interface Interface1
+{
+    string MyInterfaceProperty { get; set; }
+
+    //string Field; //- error
+    public void Foo()
+    {
+        Console.WriteLine("Foo");
+    }
+}
+
+internal interface Interface2
+{
+    public static void StaticFoo()
+    {
+        Console.WriteLine("Static foo");
+    }
+
+    public void FooInterface2();
+}
---
+++ InterviewQuestions/SampleClass.cs
@@ -0,0 +1,21 @@
+﻿namespace InterviewQuestions;
+
+internal class SampleClass : AbstractClass, Interface1, Interface2
+{
+    public void Foo() // optional
+    {
+        Console.WriteLine("SampleClass Foo");
+    }
+
+    public string MyInterfaceProperty { get; set; }
+
+    public void FooInterface2()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void FooClass()
+    {
+        throw new NotImplementedException();
+    }
+}
