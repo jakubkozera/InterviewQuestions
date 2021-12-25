2723321f201a771f1420ec44233ff7dfde4a0d44
Jakub Kozera <jakubkozera@outlook.com>
2021-12-25 09:17:01 +0100

    Sample

--- InterviewQuestions/Program.cs
+++ InterviewQuestions/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿BaseClass someClass = new SomeClass();
+
+someClass.FooVirtual();
+
+someClass.Foo();
+
+internal class BaseClass
+{
+    public virtual void FooVirtual()
+    {
+        Console.WriteLine("FooVirtual base");
+    }
+
+    public void Foo()
+    {
+        Console.WriteLine("Foo base");
+    }
+}
+
+internal class SomeClass : BaseClass
+{
+    public override void FooVirtual()
+    {
+        Console.WriteLine("SomeClass FooVirtual");
+    }
+
+    public new void Foo()
+    {
+        Console.WriteLine("Foo");
+    }
+}
