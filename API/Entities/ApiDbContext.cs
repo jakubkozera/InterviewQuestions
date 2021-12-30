0eabeae34302ff1a2b157ec53ccd78818492e172
Jakub Kozera <jakubkozera@outlook.com>
2021-12-30 16:52:19 +0100

    setup

---
+++ API/Entities/ApiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities;
+
+public class ApiDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ApiDb;Trusted_Connection=True;");
+}
---
+++ API/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
