c3d97609a9985b25db91880c4e61f85a91e78af8
Jakub Kozera <jakubkozera@outlook.com>
2021-12-31 07:52:42 +0100

    second migration

---
+++ API/Migrations/20211231064812_UserEmailAdded.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class UserEmailAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+        }
+    }
+}
